fff0c51fef34d7cba98da7b72c62b733f703b0a7
Justin <jhighland99@gmail.com>
2019-01-22 20:18:11 -0800

    Add Material UI

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Switch from 'react-router-dom/Switch'
+import Switch from 'react-router-dom/Switch';
+import { MuiThemeProvider } from '@material-ui/core';
+import { theme } from './theme';
 
 const devTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
@@ -16,13 +18,13 @@ const store = createStore(rootReducer, devTools);
 
 render(
  <Provider store={store}>
-   {/* <MuiThemeProvider theme={theme}> */}
+   <MuiThemeProvider theme={theme}>
      <BrowserRouter>
        <Switch>
          <Route exact path='/' component={App} />
        </Switch>
      </BrowserRouter>
-   {/* </MuiThemeProvider> */}
+   </MuiThemeProvider>
  </Provider>,
  document.getElementById('root')
 );
