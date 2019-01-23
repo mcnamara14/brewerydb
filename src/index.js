3d334d6946dac424755ce2c332bc83e581b99af4
Justin <jhighland99@gmail.com>
2019-01-22 19:31:49 -0800

    Add store to index file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import rootReducer from './reducers';
+
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
