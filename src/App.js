6793c0b15be0fbe9b277b177828eeddcdfaf908f
Justin <jhighland99@gmail.com>
2019-01-28 20:33:24 -0800

    Add button

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import './App.css';
 import { Button } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid'
 import Input from '@material-ui/core/Input'
+import withStyles from '@material-ui/core/styles/withStyles'
 
-class App extends Component {
-  render() {
-    return (
-      <Grid container justify="center">
-        <Grid item xs={10}><input>Search for breweries here...</input></Grid>
-      </Grid>
-    );
+
+const styles = {
+  container: {
+    background: "#eee",
+  },
+  input: {
+    width: "100%",
+    height: 50
+  },
+  button: {
+    width: "100%",
+    marginTop: 20
   }
 }
 
-export default App;
+class App extends Component {
+  render() {
+    const {classes} = this.props
+    return <Grid container justify="center">
+        <Grid item className={classes.container} xs={6}>
+          <Input className={classes.input} />
+          <Button color="secondary" variant="contained" className={classes.button}>Find Breweries</Button>
+        </Grid>
+      </Grid>;
+  }
+}
+
+export default withStyles(styles)(App);
